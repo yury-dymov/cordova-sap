643da1cd8f7d62700d88eb94e2647808259a1ce4
Yury Dymov <yuri.dymov@sap.com>
2016-08-04 09:27:51 +0800

    fix

--- src/ios/CDVSap.m
+++ src/ios/CDVSap.m
@@ -1,7 +1,11 @@
 #import "CDVSap.h"
 
+@implementation Sap
+
 - (void)sapHelloWorld:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Hello World"];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+@end
